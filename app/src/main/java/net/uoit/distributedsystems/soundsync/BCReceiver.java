86603570af4e166f82f0ff6fc3686dbc02e80ac8
mysteryhobo <thepeterlittle@gmail.com>
2015-10-28 12:20:12 -0400

    added onReceive

--- app/src/main/java/net/uoit/distributedsystems/soundsync/BCReceiver.java
+++ app/src/main/java/net/uoit/distributedsystems/soundsync/BCReceiver.java
@@ -3,6 +3,8 @@ package net.uoit.distributedsystems.soundsync;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.util.Log;
 
 /**
  * Created by ubuntu on 28/10/15.
@@ -11,6 +13,14 @@ public class BCReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        //TODO Receive things
+        if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
+            // Request available peers from the wifi p2p manager. This is an
+            // asynchronous call and the calling activity is notified with a
+            // callback on PeerListListener.onPeersAvailable()
+            if (mManager != null) {
+                mManager.requestPeers(mChannel, peerListListener);
+            }
+            Log.d(WiFiDirectActivity.TAG, "P2P peers changed");
+        }
     }
 }
