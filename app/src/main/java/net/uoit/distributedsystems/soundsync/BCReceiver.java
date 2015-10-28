cd1e2c65127de3ff99654c245530f0d91945e9c1
mysteryhobo <thepeterlittle@gmail.com>
2015-10-28 11:41:36 -0400

    added broadcast receiver activity and receiver class

---
+++ app/src/main/java/net/uoit/distributedsystems/soundsync/BCReceiver.java
@@ -0,0 +1,16 @@
+package net.uoit.distributedsystems.soundsync;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by ubuntu on 28/10/15.
+ */
+public class BCReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        //TODO Receive things here
+    }
+}
