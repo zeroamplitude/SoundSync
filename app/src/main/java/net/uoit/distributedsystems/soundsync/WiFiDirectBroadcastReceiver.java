0b69cd1575a2f1c37f326e2d5f52dc9d62a543a2
ncdesouza <nicholas.desouza@uoit.net>
2015-10-28 13:16:16 -0400

    added DiscoverPeersClass

--- app/src/main/java/net/uoit/distributedsystems/soundsync/WiFiDirectBroadcastReceiver.java
+++ app/src/main/java/net/uoit/distributedsystems/soundsync/WiFiDirectBroadcastReceiver.java
@@ -3,7 +3,6 @@ package net.uoit.distributedsystems.soundsync;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.net.wifi.p2p.WifiP2pDevice;
 import android.net.wifi.p2p.WifiP2pManager;
 
 import java.nio.channels.Channel;
