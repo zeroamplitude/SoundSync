4709feddb5ead09fccbf7601604185c62aec73bb
ncdesouza <nicholas.desouza@uoit.net>
2015-11-10 12:56:41 -0500

    added PeerViewHolder Class

---
+++ app/src/main/java/net/uoit/distributedsystems/soundsync/PeerViewHolder.java
@@ -0,0 +1,13 @@
+package net.uoit.distributedsystems.soundsync;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by nicholas on 10/11/15.
+ */
+public class PeerViewHolder extends RecyclerView.ViewHolder {
+    public PeerViewHolder(View itemView) {
+        super(itemView);
+    }
+}
