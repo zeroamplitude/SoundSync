e7919f1d6fe8ec4f66d6043a9a083f8b49f78eb9
ncdesouza <nicholas.desouza@uoit.net>
2015-11-10 12:55:42 -0500

    added Peer Class

--- app/src/main/java/net/uoit/distributedsystems/soundsync/ConnectPeerActivity.java
+++ app/src/main/java/net/uoit/distributedsystems/soundsync/ConnectPeerActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
@@ -24,6 +26,11 @@ public class ConnectPeerActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        RecyclerView peerlist = (RecyclerView) findViewById(R.id.peerlist);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        peerlist.setLayoutManager(linearLayoutManager);
     }
 
 }
---
+++ app/src/main/java/net/uoit/distributedsystems/soundsync/Peer.java
@@ -0,0 +1,11 @@
+package net.uoit.distributedsystems.soundsync;
+
+/**
+ * Created by nicholas on 10/11/15.
+ */
+public class Peer {
+
+    protected String name;
+    protected String address;
+
+}
