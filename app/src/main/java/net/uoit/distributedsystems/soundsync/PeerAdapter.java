0c19244fcf7b30e902907d23d7eb84213d9ea367
mysteryhobo <thepeterlittle@gmail.com>
2015-11-10 13:15:36 -0500

    added ConnectPeer activity and renamed wifiDirect activity to connect host activity

--- app/src/main/java/net/uoit/distributedsystems/soundsync/PeerAdapter.java
+++ app/src/main/java/net/uoit/distributedsystems/soundsync/PeerAdapter.java
@@ -1,9 +1,11 @@
 package net.uoit.distributedsystems.soundsync;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
-import java.util.List;
 
+import java.util.List;
 
 /**
  * Created by ubuntu on 10/11/15.
@@ -17,12 +19,15 @@ public class PeerAdapter extends RecyclerView.Adapter<PeerViewHolder> {
 
     @Override
     public PeerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_layout, parent, false);
+        return new PeerViewHolder(itemView);
     }
 
     @Override
     public void onBindViewHolder(PeerViewHolder holder, int position) {
-
+        Peer p = peerList.get(position);
+        PeerViewHolder.vName.setText(p.name);
+        PeerViewHolder.vAddress.setText(p.address);
     }
 
     @Override
